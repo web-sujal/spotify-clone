87ce3daf085b945a1935d46c1084d5544e2eb7d6
Sujal <web.sujal@gmail.com>
2024-03-17 16:40:12 +0530

    add useUploadModal store

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,9 +3,23 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+
   const onClick = () => {
-    // TODO: handle upload later
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    // TODO: check for subscription
+
+    uploadModal.onOpen();
   };
 
   return (
