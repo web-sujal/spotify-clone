cc7b464c6f06b65486ce304d67f53d800d6957df
Sujal <web.sujal@gmail.com>
2024-04-05 14:06:34 +0530

    add player to home page and
    - add useOnPlay hook
    - add radix ui slider to player for volume control
    -

--- components/Player.tsx
+++ components/Player.tsx
@@ -4,6 +4,8 @@ import useGetSongById from "@/hooks/useGetSongById";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
 
+import PlayerContent from "./PlayerContent";
+
 const Player = () => {
   const player = usePlayer();
   const { song } = useGetSongById(player.activeId);
@@ -16,7 +18,7 @@ const Player = () => {
 
   return (
     <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
-      Player
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   );
 };
