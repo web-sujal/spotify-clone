505aaa78ba1ed0849c5121da678fc86f3a093c74
Sujal <web.sujal@gmail.com>
2024-04-07 12:27:07 +0530

    update getURL fn to return valid url

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -5,4 +5,9 @@ export const getURL = () => {
     process.env.NEXT_PUBLIC_SITE_URL ??
     process.env.NEXT_PUBLIC_VERCEL_URL ??
     "http://localhost:3000/";
+
+  url = url.includes("http") ? url : `https://${url}`;
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+
+  return url;
 };
