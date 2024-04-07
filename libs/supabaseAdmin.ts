93473e0c6527194caeffd078cda171e2e4ef8268
Sujal <web.sujal@gmail.com>
2024-04-07 13:20:51 +0530

    add supabaseAdmin and add upsertProductRecord helper fn

---
+++ libs/supabaseAdmin.ts
@@ -0,0 +1,32 @@
+import Stripe from "stripe";
+import { createClient } from "@supabase/supabase-js";
+
+import { Database } from "@/types_db";
+import { Price, Product } from "@/types";
+
+import { stripe } from "./stripe";
+import { toDateTime } from "./helpers";
+
+export const supabaseAdmin = createClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ""
+);
+
+const upsertProductRecord = async (product: Stripe.Product) => {
+  const productData: Product = {
+    id: product.id,
+    active: product.active,
+    name: product.name,
+    description: product.description ?? undefined,
+    image: product.images?.[0] ?? null,
+    metadat: product.metadata,
+  };
+
+  const { error } = await supabaseAdmin.from("products").upsert([productData]);
+
+  if (error) {
+    throw error;
+  }
+
+  console.log("Product inserted/updated: ", product.id);
+};
