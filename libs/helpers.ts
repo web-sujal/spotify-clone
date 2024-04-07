c733cc36e85cd0c0b52eb300930701736f2598ec
Sujal <web.sujal@gmail.com>
2024-04-07 12:58:41 +0530

    add toDateTime helper fn

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -36,3 +36,10 @@ export const postData = async ({
 
   return res.json();
 };
+
+export const toDateTime = (secs: number) => {
+  var t = new Date("1970-01-01T00:30:00Z");
+  t.setSeconds(secs);
+
+  return t;
+};
