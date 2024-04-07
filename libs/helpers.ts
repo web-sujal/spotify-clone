d9202140939b3b295d5e124d6c2efb18523531c2
Sujal <web.sujal@gmail.com>
2024-04-07 12:56:09 +0530

    add postData helper fn

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -11,3 +11,28 @@ export const getURL = () => {
 
   return url;
 };
+
+export const postData = async ({
+  url,
+  data,
+}: {
+  url: string;
+  data?: { price: Price };
+}) => {
+  console.log("POST REQUEST: ", url, data);
+
+  const res: Response = await fetch(url, {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    credentials: "same-origin",
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    console.log("Error in POST", { url, data, res });
+
+    throw new Error(res.statusText);
+  }
+
+  return res.json();
+};
