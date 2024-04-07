9a971803593e94490dded8b5e3fc1e0f149fd900
Sujal <web.sujal@gmail.com>
2024-04-07 12:22:45 +0530

    add getURL helper fn

---
+++ libs/helpers.ts
@@ -0,0 +1,8 @@
+import { Price } from "@/types";
+
+export const getURL = () => {
+  let url =
+    process.env.NEXT_PUBLIC_SITE_URL ??
+    process.env.NEXT_PUBLIC_VERCEL_URL ??
+    "http://localhost:3000/";
+};
