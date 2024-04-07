7e29225cb4617dd7186ed2dc5b9934501469f20a
Sujal <web.sujal@gmail.com>
2024-04-08 00:07:31 +0530

    add createOrRetrieveACustomer helper fn

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -54,3 +54,40 @@ const upsertPriceRecord = async (price: Stripe.Price) => {
 
   console.log("Price inserted/updated: ", price.id);
 };
+
+const createOrRetrieveACustomer = async ({
+  email,
+  uuid,
+}: {
+  email: string;
+  uuid: string;
+}) => {
+  const { data, error } = await supabaseAdmin
+    .from("customers")
+    .select("stripe_customer_id")
+    .eq("id", uuid)
+    .single();
+
+  if (error || !data?.stripe_customer_id) {
+    const customerData: { metadata: { supabaseUUID: string }; email?: string } =
+      {
+        metadata: { supabaseUUID: uuid },
+      };
+
+    if (email) customerData.email = email;
+
+    const customer = await stripe.customers.create(customerData);
+    const { error: supabaseError } = await supabaseAdmin
+      .from("customers")
+      .insert([{ id: uuid, stripe_customer_id: customer.id }]);
+
+    if (supabaseError) {
+      throw supabaseError;
+    }
+
+    console.log("New customer created and inserted for ", uuid);
+    return customer.id;
+  }
+
+  return data?.stripe_customer_id;
+};
